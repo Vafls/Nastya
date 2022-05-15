80e1667e8aebc616e84cef281a1bc663d9db24c7
Vafls <87562076+Vafls@users.noreply.github.com>
2022-05-16 00:07:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import {PageFlip} from 'page-flip';
+const pageFlip = new PageFlip(document.getElementById('book'),
+    {
+        width: 400, // required parameter - base page width
+        height: 600  // required parameter - base page height
+    }
+);
+
+pageFlip.loadFromHTML(document.querySelectorAll('.my-page'));
